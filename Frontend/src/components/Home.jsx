e0811b227bcf7be1e283559816ad5e34a220776d
Alvaro Debernardi <alvarodebernardi1@gmail.com>
2025-09-17 14:03:05 -0300

    Refactor: making home buttons buttons, and styling

--- Frontend/src/components/Home.jsx
+++ Frontend/src/components/Home.jsx
@@ -1,34 +1,35 @@
-import "./style/Home.css"
+import { Link } from 'react-router-dom'
 import Header from "./Header.jsx"
+import "./style/Home.css"
 
 export default function Home() {
     const play = { 
         name: "Play",
-        desc: "Play against our ML model."
+        desc: "Play against our ML model.",
+        to: "/play"
     }
     const predict = { 
         name: "Predict",
-        desc: "Give a galaxy image to our ML model to see its morphology."
+        desc: "Give a galaxy image to our ML model to see its morphology.",
+        to: "/predict"
     }
 
     return (
         <div className="home">
             <h1> ChadIA- Galaxy Classifier </h1>
             <div className="home-applications">
-                <ApplicationButton name={play.name} desc={play.desc} />
-                <ApplicationButton name={predict.name} desc={predict.desc} />
+                <ApplicationButton name={play.name} desc={play.desc} to={play.to}/>
+                <ApplicationButton name={predict.name} desc={predict.desc} to={predict.to}/>
             </div>
         </div>
     )
 }
 
-function ApplicationButton({ name, desc }) {
+function ApplicationButton({ name, desc, to }) {
     return (
-        <div className="blur-background">
+        <Link to={to} className="blur-background">
             <h3> {name} </h3>
-            <p>
-                {desc}
-            </p>
-        </div>
+            <p> {desc} </p>
+        </Link>
     )
 }
