8400de7813719c1701d556f74ee06d1324182fa0
Leon <leondellafiore2016@gmail.com>
2025-09-18 18:31:57 -0300

    Refactor About add Style

--- Frontend/src/components/About.jsx
+++ Frontend/src/components/About.jsx
@@ -4,9 +4,46 @@ import "./style/About.css"
 export default function About() {
     return (
         <div className="about">
+                <div className="mldiv">
+                    <h1>Machine Learning Model</h1>
+                    <p> Una CNN (Convolutional Neural Network) es un modelo de machine learning especializado en procesar datos con estructura de grid, como imágenes.<br></br>Su idea principal es usar capas convolucionales que aplican filtros (o kernels) para extraer automáticamente características locales (bordes, texturas, formas), reduciendo la necesidad de diseñarlas manualmente. Luego, con capas de pooling, se resume la información más importante reduciendo la dimensionalidad. Finalmente, se usan capas totalmente conectadas (fully connected) para clasificar o predecir según las características aprendidas.</p>
+                </div>
+                <div className="howitwork">
+                    <h1>How it works</h1>
+                    <div id="descriptionQuestion">
+                        <div className="description">
+                            <p>Brief description of hot the model works on dataset</p>
+                        </div>
+                        <div className="questionsTable">
+                            <table>
+                                <tr>
+                                    <th>
+                                        <p>Task</p>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <p>Question</p>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <p>Response</p>
+                                    </th>
+                                </tr>
+                                <tr>
+                                    <th>
+                                        <p>Next</p>
+                                    </th>
+                                </tr>
+                            </table> 
+                            <div>
+                                <p>Continuacion de descripcion</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             
-            <h1> Hello About, with docker smooth tooned </h1>
-            <h1> ChadIA- Galaxy Classifier </h1>
             <div style={{ marginTop: "2rem" }}>
                 <LinkToGalaxyZoo />
             </div>
