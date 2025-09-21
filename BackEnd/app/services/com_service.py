024fa36068da55b587bd894bafff446b93ca6456
4ndres90 <venierandres@gmail.com>
2025-09-21 15:46:11 -0300

    Minor tweaks on com_service and validation_service

--- BackEnd/app/services/com_service.py
+++ BackEnd/app/services/com_service.py
@@ -1,15 +1,17 @@
-from file_storage_service import FileStorageService
+import requests
+from .file_storage_service import FileStorageService
+
 
 class ComService:
-    def __init__(self, ml_engine, db_service, storage_service):
+    def __init__(self, db_service, storage_service):
         
-        self.ml_engine = ml_engine
+        #self.ml_engine = ml_engine
         
         self.db_service = db_service
         
         self.storage_service = storage_service
         
-    def process(self, image):
+    def process(self, image_file):
 
         filename = getattr(image_file, "filename", "upload.bin")
         mimetype = getattr(image_file, "mimetype", "application/octet-stream")
@@ -34,17 +36,26 @@ class ComService:
         #Rewind para guardar con tu storage actual 
         try:
             image_file.stream.seek(0)
-            image_path = self.storage_service.save(image_file)
+            image_bytes = image_file.stream.read()
+            image_path = self.storage_service.save(image_bytes)
         except Exception:
 
             raise
 
         # Guardar predicción completa en DB 
+        saved_image = self.db_service.save_prediction(image_path, features)
 
         # Buscar una imagen cualquiera que cumpla esos features
-
+        similar_images = self.db_service.search_by_features(features)
+        
+        # excluimos la imagen recien cargada
+        similar_images = [img for img in similar_images if img.id != saved_image.id]
+        
+        # guardamos la ruta de la primer imagen que cumpla con las caracteristicas (si existe)
+        similar_image_filename = similar_images[0].filename if similar_images else None
+        
         # Devolver tal cual al caller
         return {
-            "prediction": ml_json,
-            "saved_image_path": image_path,
+            "similar_image_filename": similar_image_filename,
+            "features": features
         }
