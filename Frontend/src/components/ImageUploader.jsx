8e30e62d62d410f402bfddaec40ef995837bbe6e
Leon <leondellafiore2016@gmail.com>
2025-10-01 16:09:51 -0300

    Refactor ImageUploader to handle error response and renderice PredictResults component with response result

--- Frontend/src/components/ImageUploader.jsx
+++ Frontend/src/components/ImageUploader.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
 import ErrorMessage from "./ErrorMessage"
+import PredictResults from "./PredictResults";
 import "./style/ImageUploader.css";
 
 export default function ImageUploader() {
   const [image, setImage] = useState(null);
   const [error, setError] = useState(null);
+  const [results, setResults] = useState(null);
 
- const handleFileSelect = (e) => {
-  const file = e.target.files[0];
-  if (file) {
-    setImage({
-      file, // ✅ importante para el upload
-      url: URL.createObjectURL(file),
-      name: file.name,
-    });
-  }
-};
+   const handleFileSelect = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setImage({
+        file, // ✅ importante para el upload
+        url: URL.createObjectURL(file),
+        name: file.name,
+      });
+    }
+  };
 
   // Enviar imágenes al backend
   const handleUpload = async () => {
@@ -39,40 +41,44 @@ export default function ImageUploader() {
         setError(data.error);
       } else {
         alert("Imágenes enviadas correctamente!");
-        setImage(null)
+        setImage(null);
+        setResults(data);
       }
       } catch (err) {
         console.error("Error en la subida:", err);
         setError(err.message); // 🔹 guarda el mensaje de error
         setImage(null)
       }
-  };
+    };
+    if (error) {
+      return <ErrorMessage message={error} onRetry={() => setError(null)} />;
+    }
+
+    if (results) {
+      return <PredictResults data={results} onBack={() => setResults(null)} />;
+    }
 
   return (
-    error ? (
-      <ErrorMessage message={error}  onRetry={() => setError(null)}/>
-    ):(
-      <div className="uploader">
-        <input
-          type="file"
-          accept="image/*"
-          onChange={handleFileSelect}
-          className="uploader-input"
-        />
+    <div className="uploader">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleFileSelect}
+        className="uploader-input"
+      />
 
-        <div className="uploader-grid">
-          {image && (
-            <div className="uploader-item">
-              <img src={image.url} alt={image.name} className="uploader-img" />
-              <span className="uploader-name">{image.name}</span>
-            </div>
-          )}
-        </div>
-        
-      <button onClick={handleUpload} className="uploader-btn">
+      <div className="uploader-grid">
+        {image && (
+          <div className="uploader-item">
+            <img src={image.url} alt={image.name} className="uploader-img" />
+            <span className="uploader-name">{image.name}</span>
+          </div>
+        )}
+      </div>
+
+      <button onClick={handleUpload} className="uploader-btn" disabled={!image}>
         Enviar al backend
       </button>
-    </div>        
-    )
+    </div>
   );
 }
