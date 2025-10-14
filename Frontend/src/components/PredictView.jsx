161d6f90ea82e95906a0f9f0c837731e18e35600
Leon <leondellafiore2016@gmail.com>
2025-10-13 21:41:36 -0300

    Add PredictView component to decouple responsabilities from ImageUploader, this will be a flux controller

---
+++ Frontend/src/components/PredictView.jsx
@@ -0,0 +1,10 @@
+import ImageUploader from "./ImageUploader";
+import ErrorMessage from "./ErrorMessage";
+import PredictResults from "./PredictResults";
+import { useState } from "react";
+
+export default function PredictView(){
+    return(
+        <></>
+    )
+}
