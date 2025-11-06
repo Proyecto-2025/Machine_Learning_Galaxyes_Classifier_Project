e5d660fb1ffb00b101afedd0c2b7cf06bf1096e8
4ndres90 <venierandres@gmail.com>
2025-11-06 20:10:32 -0300

    Change ML engine url in services/com_service.py

--- BackEnd/app/services/com_service.py
+++ BackEnd/app/services/com_service.py
@@ -7,7 +7,7 @@ class ComService:
         """
         Recibe un io.BytesIO con la imagen
         """
-        ml_engine_url = "http://localhost:5001/predict"
+        ml_engine_url = "https://galaxy-classifier-279086139631.us-central1.run.app/predict"
 
         try:
             img = Image.open(image_bytes_io)
@@ -16,7 +16,7 @@ class ComService:
             img_bytes_io.seek(0)
 
             files = {"image": ("uploaded_image.jpg", img_bytes_io, "image/jpeg")}
-            response = requests.post(ml_engine_url, files=files, timeout=10)
+            response = requests.post(ml_engine_url, files=files, timeout=100)
             response.raise_for_status()
 
             return response.json()
